8e88e4790e5288b4d537dfc87e996e83af4a753e
Josh Folsom <josh.folsom@yahoo.com>
2020-09-25 21:24:06 -0700

    Added character traits

---
+++ Calculations/SocialEncounterHandler.cs
@@ -0,0 +1,6 @@
+public class SocialEncounterHandler
+{
+    //take in relevant encounter info
+    //run through some calculations
+    //return the weights etc
+}
---
+++ Units/Social/CharacterTraits.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+public class CharacterTraits
+{
+    private List<CharacterTrait> traits = new List<CharacterTrait>();
+
+    public List<CharacterTrait> Traits { get => traits; set => traits = value; }
+}
